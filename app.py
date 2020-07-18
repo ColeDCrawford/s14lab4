980603d584c6a9b0bb80450f9989ae463bca7ec8
Cole Crawford <cole_crawford@harvard.edu>
2020-07-18 13:00:14 -0400

    Switch to Heroku db

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ from os import environ
 
 load_dotenv('.env')
 
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/homework_users_db'
+from flask_heroku import Heroku
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/homework_users_db'
+heroku = Heroku(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = environ.get('SECRET_KEY')
 Db.init_app(app)
